4235cafa12f3fe337a7461a6df0d61c2a65cb9b9
Tri H. Nguyen <thn.software@gmail.com>
2016-06-21 19:00:48 -0400

    adjusted variable name

--- script-hadoop/stand-alone/bin/sys/setup-ssh_share.sh
+++ script-hadoop/stand-alone/bin/sys/setup-ssh_share.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+ #!/bin/bash
 
 CWD=$(cd "$(dirname "$0")" && pwd)
 . $CWD/setenv.sh
 
 while read line
 do
-  HOST_NAME=line
-  echo "copy file to $USER_NAME@$HOSTNAME"
+  HOST_NAME=$line
+  echo "copy file to $USER_NAME@$HOST_NAME"
   ssh-copy-id -i $HOME/.ssh/id_rsa.pub $USER_NAME@$HOST_NAME
 done < $1
